54ff2c43bb6b61ee99503fe2820d179fa1b5b8d8
mevans <mevans@processflows.co.uk>
2018-06-27 21:58:40 +0100

    Added Logical Palette class

---
+++ src/com/plus/mevanspn/ui/sprite/Tile.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.plus.mevanspn.ui.sprite;
+
+/**
+ *
+ * @author win10
+ */
+final public class Tile {
+    private short width, height;
+    private LogicalPalette mode_palette;
+}
