f01a2487068744f938a78585e79a7658b914bd3a
gittymo <36928041+gittymo@users.noreply.github.com>
2020-02-17 09:14:38 +0000

    Finally added RTI and RTS mnemonics and added utility methods for pulling and pushing status register (flags) and program counter to the stack.

---
+++ BBIde/src/main/java/com/plus/mevanspn/bridge/Processor/OpCodes/Branching/RTS.java
@@ -0,0 +1,43 @@
+package com.plus.mevanspn.bridge.Processor.OpCodes.Branching;
+
+import com.plus.mevanspn.bridge.Processor.OpCode;
+import com.plus.mevanspn.bridge.Storage.RAM.*;
+
+import java.util.Objects;
+
+/** The RTS class allows for the creation of RTS (ReRurn from Subroutine) mnemonic objects within a
+ * BBIDE pseudo program.
+ * The RTS mnemonic is used to terminate a subroutine which was reached using the JSR mnemonic.  When the JSR
+ * instruction is called, the program counter register value is pushed to the stack.  When RTS is called, this value is
+ * retrieved from the stack, incremented by one and stored as offset to the next executable instruction.
+ * No flags are affected by this call.
+ */
+public class RTS extends OpCode {
+
+	@Override
+	public int[] getASM() {
+		return new int[] { 0x60 };
+	}
+
+	@Override
+	public int getSize() {
+		return 1;
+	}
+
+	@Override
+	public int getBaseCycles() {
+		return 6;
+	}
+
+	@Override
+	public void perform(Memory memory) throws InvalidAddressModeException, InvalidAddressException,
+		MemoryMissingException, StackOverflowException
+	{
+		// Make sure we've got a valid memory object
+		if (memory == null) throw new MemoryMissingException();
+		// Pull the program counter from the stack
+		memory.stack.pullPC();
+		// Increment the program counter by one.
+		memory.registers.replace("PC", memory.registers.get("PC") + 1);
+	}
+}
